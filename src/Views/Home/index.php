86ec228e58d60e8af8909c4c6ce2f4040f489db5
Davi Klayver <davi.klayveR@live.com>
2023-10-01 19:38:09 -0300

    feature-footer-carrossel

--- src/Views/Home/index.php
+++ src/Views/Home/index.php
@@ -1,1 +1,23 @@
 <?php
+
+use DevFashion\Core\Functions;
+
+?>
+
+<html>
+<head>
+
+    <?php
+        Functions::addFavicon();
+        Functions::addStyleSheet();
+    ?>
+</head>
+<body>
+<?php Functions::renderMenu(); ?>
+
+<?php require_once "Home/include/carrousel.php"; ?>
+
+<?php Functions::renderFooter(); ?>
+<?php Functions::addScript(); ?>
+</body>
+</html>
