0628cb1f7adec16affb03c40258589780a8d9c4b
Davi Klayver <davi.klayveR@live.com>
2023-10-02 22:03:27 -0300

    style-footer

--- src/Views/Sistema/footer.php
+++ src/Views/Sistema/footer.php
@@ -1,123 +1,122 @@
 <footer class="bg-dark text-center text-white fixed-bottom">
-    <!-- Grid container -->
-    <div class="container p-4">
-        <!-- Section: Social media -->
-        <section class="mb-4">
-            <!-- Facebook -->
-            <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
-            ><i class="fab fa-facebook"></i
-                ></a>
+	<!-- Grid container -->
+	<div class="container p-4">
+		<!-- Section: Social media -->
+		<section class="mb-4">
+			<!-- Facebook -->
+			<a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+			><i class="fab fa-facebook"></i
+				></a>
 
-            <!-- Twitter -->
-            <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
-            ><i class="fab fa-twitter"></i
-                ></a>
+			<!-- Twitter -->
+			<a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+			><i class="fab fa-twitter"></i
+				></a>
 
-            <!-- Google -->
-            <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
-            ><i class="fab fa-google"></i
-                ></a>
+			<!-- Google -->
+			<a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+			><i class="fab fa-google"></i
+				></a>
 
-            <!-- Instagram -->
-            <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
-            ><i class="fab fa-instagram"></i
-                ></a>
+			<!-- Instagram -->
+			<a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+			><i class="fab fa-instagram"></i
+				></a>
 
-            <!-- Linkedin -->
-            <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
-            ><i class="fab fa-linkedin-in"></i
-                ></a>
+			<!-- Linkedin -->
+			<a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+			><i class="fab fa-linkedin-in"></i
+				></a>
 
-            <!-- Github -->
-            <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
-            ><i class="fab fa-github"></i
-                ></a>
-        </section>
-        <!-- Section: Social media -->
+			<!-- Github -->
+			<a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+			><i class="fab fa-github"></i
+				></a>
+		</section>
+		<!-- Section: Social media -->
 
-        <!-- Section: Form -->
-        <section class="">
-            <form action="">
-                <!--Grid row-->
-                <div class="row d-flex justify-content-center">
-                    <!--Grid column-->
-                    <div class="col-auto">
-                        <p class="pt-2">
-                            <strong>Inscreva-se em nossa newsletter</strong>
-                        </p>
-                    </div>
-                    <!--Grid column-->
+		<!-- Section: Form -->
+		<section class="">
+			<form action="">
+				<!--Grid row-->
+				<div class="row d-flex justify-content-center">
+					<!--Grid column-->
+					<div class="col-auto">
+						<p class="pt-2">
+							<strong>Inscreva-se em nossa newsletter</strong>
+						</p>
+					</div>
+					<!--Grid column-->
 
-                    <!--Grid column-->
-                    <div class="col-md-5 col-12">
-                        <!-- Email input -->
-                        <div class="form-outline form-white mb-3">
-                            <input type="email" id="form5Example21" class="form-control" placeholder="Insira seu e-mail"/>
-<!--                            <label class="form-label" for="form5Example21">Endereço de email</label>-->
-                        </div>
-                    </div>
-                    <!--Grid column-->
+					<!--Grid column-->
+					<div class="col-md-5 col-12">
+						<!-- Email input -->
+						<div class="form-outline form-white mb-3">
+							<input type="email" id="form5Example21" class="form-control" placeholder="Insira seu e-mail"/>
+							<!--                            <label class="form-label" for="form5Example21">Endereço de email</label>-->
+						</div>
+					</div>
+					<!--Grid column-->
 
-                    <!--Grid column-->
-                    <div class="col-auto">
-                        <!-- Submit button -->
-                        <button type="submit" class="btn btn-outline-light mb-4">
-                            Inscrever
-                        </button>
-                    </div>
-                    <!--Grid column-->
-                </div>
-                <!--Grid row-->
-            </form>
-        </section>
-        <!-- Section: Form -->
+					<!--Grid column-->
+					<div class="col-auto">
+						<!-- Submit button -->
+						<button type="submit" class="btn btn-outline-light mb-4">
+							Inscrever
+						</button>
+					</div>
+					<!--Grid column-->
+				</div>
+				<!--Grid row-->
+			</form>
+		</section>
+		<!-- Section: Form -->
 
-        <!-- Section: Text -->
-        <section class="mb-4">
-            <p>
-                Site desenvolvido pelo Grupo 1 da matéria de Desenvolvimento Web: HTML, CSS, Javascript e PHP; Ministrada pelo Professor André Teixeira. <br>
-                Integrantes do Grupo: Davi Klayver | Fracisco Santos | Gabriel Vieira | Johon Lennon | Juliana Rezende
-            </p>
-        </section>
-        <!-- Section: Text -->
+		<!-- Section: Text -->
+		<section class="mb-4">
+			<p>
+				Site desenvolvido pelo Grupo 1 da matéria de Desenvolvimento Web: HTML, CSS, Javascript e PHP; Ministrada pelo Professor André Teixeira. <br>
+				Integrantes do Grupo: Davi Klayver | Fracisco Santos | Gabriel Vieira | Johon Lennon | Juliana Rezende
+			</p>
+		</section>
+		<!-- Section: Text -->
 
-        <!-- Section: Links -->
-        <section class="">
-            <!--Grid row-->
-            <div class="row">
-                <!--Grid column-->
+		<!-- Section: Links -->
+		<section class="">
+			<!--Grid row-->
+			<div class="row">
+				<!--Grid column-->
 
-                <!--Grid column-->
-                <div class="col-lg-0 col-md-0 mb-0 mb-md-2">
-                    <h5 class="text-uppercase">Links</h5>
+				<!--Grid column-->
+				<div class="col-lg-0 col-md-0 mb-0 mb-md-2">
+					<h5 class="text-uppercase">Links</h5>
 
-                    <ul class="list-unstyled mb-0">
-                        <li>
-                            <a href="#!" class="text-white">Fale Conosco</a>
-                        </li>
-                        <li>
-                            <a href="#!" class="text-white">Trabalhe Conosco</a>
-                        </li>
-                        <li>
-                            <a href="#!" class="text-white">Sobre</a>
-                        </li>
-                        <li>
-                            <a href="#!" class="text-white">Ajuda</a>
-                        </li>
-                    </ul>
-                <!--Grid column-->
-            </div>
-            <!--Grid row-->
-        </section>
-        <!-- Section: Links -->
-    </div>
-    <!-- Grid container -->
+					<ul class="list-unstyled mb-0">
+						<li>
+							<a href="#!" class="text-white">Fale Conosco</a>
+						</li>
+						<li>
+							<a href="#!" class="text-white">Trabalhe Conosco</a>
+						</li>
+						<li>
+							<a href="#!" class="text-white">Sobre</a>
+						</li>
+						<li>
+							<a href="#!" class="text-white">Ajuda</a>
+						</li>
+					</ul>
+					<!--Grid column-->
+				</div>
+				<!--Grid row-->
+		</section>
+		<!-- Section: Links -->
+	</div>
+	<!-- Grid container -->
 
-    <!-- Copyright -->
-    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-        © 2020 Copyright:
-        <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
-    </div>
-    <!-- Copyright -->
+	<!-- Copyright -->
+	<div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+		© 2020 Copyright:
+		<a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+	</div>
+	<!-- Copyright -->
 </footer>
-<!-- Footer -->
